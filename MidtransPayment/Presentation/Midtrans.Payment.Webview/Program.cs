5bf4b0908b0e9146b2217033905d11443abecb50
GioIsa <gioisa161@gmail.com>
2023-06-17 07:58:40 +0700

    ganti template , anjir emang

--- MidtransPayment/Presentation/Midtrans.Payment.Webview/Program.cs
+++ MidtransPayment/Presentation/Midtrans.Payment.Webview/Program.cs
@@ -2,14 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddMvc().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	builder.Services.AddControllersWithViews();
-    builder.Services.AddMvc().AddRazorRuntimeCompilation();
+	
 
 	app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
---
+++ MidtransPayment/Presentation/Midtrans.Payment.Webview/wwwroot/main-script-page/homepage/homepage.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    var owl = $('#owl-carousel');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true, autoHeight: true,
+        navText: ['<i class="las la-angle-left"></i>', '<i class="las la-angle-right"></i>'], 
+    });
+    owl.on('mousewheel', '.owl-stage', function (e, delta) {
+        console.log(delta)
+        if (delta > 0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
+
+});
+
+//SCRIPT FOR SCROLLED PAGE CHANGE BACKGROUN NAVBAR
+const myNav = $('#navbar-scrolled');
+window.onscroll = function () {
+    if (window.pageYOffset >= 100) {
+        myNav.addClass("scrolled");
+        myNav.removeClass("bg-transparent");
+    }
+    else {
+        myNav.addClass("bg-transparent");
+        myNav.removeClass("scrolled");
+    }
+};
